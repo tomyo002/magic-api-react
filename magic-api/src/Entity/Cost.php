5de291a962419a8cb26609c7471277facd33c437
Daret Tom <daret.tom@gmail.com>
2024-02-19 18:49:42 +0100

    creating the Cost entity

---
+++ magic-api/src/Entity/Cost.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CostRepository::class)]
+class Cost
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'costs')]
+    private ?Card $card = null;
+
+    #[ORM\ManyToOne(inversedBy: 'costs')]
+    private ?Element $element = null;
+
+    #[ORM\Column]
+    private ?int $number = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCard(): ?Card
+    {
+        return $this->card;
+    }
+
+    public function setCard(?Card $card): static
+    {
+        $this->card = $card;
+
+        return $this;
+    }
+
+    public function getElement(): ?Element
+    {
+        return $this->element;
+    }
+
+    public function setElement(?Element $element): static
+    {
+        $this->element = $element;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): static
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+}
