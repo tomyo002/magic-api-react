6f56189bab35fbffc2def5d921496e0025a35383
Daret Tom <daret.tom@gmail.com>
2024-02-19 18:49:54 +0100

    creating the Extension entity

---
+++ magic-api/src/Entity/Extension.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExtensionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExtensionRepository::class)]
+class Extension
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $releaseYear = null;
+
+    #[ORM\OneToMany(targetEntity: Card::class, mappedBy: 'extension')]
+    private Collection $cards;
+
+    public function __construct()
+    {
+        $this->cards = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getReleaseYear(): ?string
+    {
+        return $this->releaseYear;
+    }
+
+    public function setReleaseYear(string $releaseYear): static
+    {
+        $this->releaseYear = $releaseYear;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Card>
+     */
+    public function getCards(): Collection
+    {
+        return $this->cards;
+    }
+
+    public function addCard(Card $card): static
+    {
+        if (!$this->cards->contains($card)) {
+            $this->cards->add($card);
+            $card->setExtension($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCard(Card $card): static
+    {
+        if ($this->cards->removeElement($card)) {
+            // set the owning side to null (unless already changed)
+            if ($card->getExtension() === $this) {
+                $card->setExtension(null);
+            }
+        }
+
+        return $this;
+    }
+}
