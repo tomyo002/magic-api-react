06292201403d9e7e99464b75d845d3a809632189
Daret Tom <daret.tom@gmail.com>
2024-02-19 18:49:35 +0100

    creating the Card entity

---
+++ magic-api/src/Entity/Card.php
@@ -0,0 +1,232 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\CardRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CardRepository::class)]
+#[ApiResource]
+class Card
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $Subtype = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $healthPoint = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $attackPoint = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $loyalty = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cards')]
+    private ?Type $type = null;
+
+    #[ORM\ManyToOne(inversedBy: 'cards')]
+    private ?Extension $extension = null;
+
+    #[ORM\OneToMany(targetEntity: Cost::class, mappedBy: 'card')]
+    private Collection $costs;
+
+    #[ORM\OneToMany(targetEntity: Capacity::class, mappedBy: 'card')]
+    private Collection $capacities;
+
+    #[ORM\ManyToMany(targetEntity: Keyword::class, mappedBy: 'card')]
+    private Collection $keywords;
+
+    public function __construct()
+    {
+        $this->costs = new ArrayCollection();
+        $this->capacities = new ArrayCollection();
+        $this->keywords = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSubtype(): ?string
+    {
+        return $this->Subtype;
+    }
+
+    public function setSubtype(string $Subtype): static
+    {
+        $this->Subtype = $Subtype;
+
+        return $this;
+    }
+
+    public function getHealthPoint(): ?int
+    {
+        return $this->healthPoint;
+    }
+
+    public function setHealthPoint(?int $healthPoint): static
+    {
+        $this->healthPoint = $healthPoint;
+
+        return $this;
+    }
+
+    public function getAttackPoint(): ?int
+    {
+        return $this->attackPoint;
+    }
+
+    public function setAttackPoint(?int $attackPoint): static
+    {
+        $this->attackPoint = $attackPoint;
+
+        return $this;
+    }
+
+    public function getLoyalty(): ?int
+    {
+        return $this->loyalty;
+    }
+
+    public function setLoyalty(?int $loyalty): static
+    {
+        $this->loyalty = $loyalty;
+
+        return $this;
+    }
+
+    public function getType(): ?Type
+    {
+        return $this->type;
+    }
+
+    public function setType(?Type $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getExtension(): ?Extension
+    {
+        return $this->extension;
+    }
+
+    public function setExtension(?Extension $extension): static
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Cost>
+     */
+    public function getCosts(): Collection
+    {
+        return $this->costs;
+    }
+
+    public function addCost(Cost $cost): static
+    {
+        if (!$this->costs->contains($cost)) {
+            $this->costs->add($cost);
+            $cost->setCard($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCost(Cost $cost): static
+    {
+        if ($this->costs->removeElement($cost)) {
+            // set the owning side to null (unless already changed)
+            if ($cost->getCard() === $this) {
+                $cost->setCard(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Capacity>
+     */
+    public function getCapacities(): Collection
+    {
+        return $this->capacities;
+    }
+
+    public function addCapacity(Capacity $capacity): static
+    {
+        if (!$this->capacities->contains($capacity)) {
+            $this->capacities->add($capacity);
+            $capacity->setCard($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCapacity(Capacity $capacity): static
+    {
+        if ($this->capacities->removeElement($capacity)) {
+            // set the owning side to null (unless already changed)
+            if ($capacity->getCard() === $this) {
+                $capacity->setCard(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Keyword>
+     */
+    public function getKeywords(): Collection
+    {
+        return $this->keywords;
+    }
+
+    public function addKeyword(Keyword $keyword): static
+    {
+        if (!$this->keywords->contains($keyword)) {
+            $this->keywords->add($keyword);
+            $keyword->addCard($this);
+        }
+
+        return $this;
+    }
+
+    public function removeKeyword(Keyword $keyword): static
+    {
+        if ($this->keywords->removeElement($keyword)) {
+            $keyword->removeCard($this);
+        }
+
+        return $this;
+    }
+}
