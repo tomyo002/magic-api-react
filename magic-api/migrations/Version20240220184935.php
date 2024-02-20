5de84ed5d6e5114beede88c05b37964d0a78f90e
Daret Tom <daret.tom@gmail.com>
2024-02-20 19:59:46 +0100

    new migration

---
+++ magic-api/migrations/Version20240220184935.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240220184935 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE icon_elt (id INT AUTO_INCREMENT NOT NULL, element_id INT DEFAULT NULL, icon LONGBLOB NOT NULL, UNIQUE INDEX UNIQ_8C2AC1931F1F2A24 (element_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE icon_elt ADD CONSTRAINT FK_8C2AC1931F1F2A24 FOREIGN KEY (element_id) REFERENCES element (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE icon_elt DROP FOREIGN KEY FK_8C2AC1931F1F2A24');
+        $this->addSql('DROP TABLE icon_elt');
+    }
+}
