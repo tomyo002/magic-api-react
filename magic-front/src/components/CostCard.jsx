9b8decb6e43a1f4b2b95a4b38006224f25c2ef1f
Tom Daret <daret.tom@gmail.com>
2024-02-20 17:29:19 +0100

    display cost list

---
+++ magic-front/src/components/CostCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Cost from "./Cost";
+
+function CostCard({ costs }) {
+  console.log(costs);
+  return costs.map((c) => <Cost cost={c} />);
+}
+export default CostCard;
+
+CostCard.propTypes = {
+  costs: PropTypes.node,
+};
