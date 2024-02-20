1895ad2800ff9e9ae2e988137feddd3e87d0b98a
Tom Daret <daret.tom@gmail.com>
2024-02-20 10:59:04 +0100

    capacity ressource api

--- magic-api/src/Entity/Capacity.php
+++ magic-api/src/Entity/Capacity.php
@@ -2,10 +2,16 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\CapacityRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CapacityRepository::class)]
+#[ApiResource]
+#[Get]
+#[GetCollection]
 class Capacity
 {
     #[ORM\Id]
