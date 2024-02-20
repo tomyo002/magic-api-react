e2a2a2cb1267850441fd04ad3fbdaf03d35c321e
Tom Daret <daret.tom@gmail.com>
2024-02-20 10:59:23 +0100

    extension ressource api

--- magic-api/src/Entity/Extension.php
+++ magic-api/src/Entity/Extension.php
@@ -2,12 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ExtensionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ExtensionRepository::class)]
+#[ApiResource]
+#[Get]
+#[GetCollection]
 class Extension
 {
     #[ORM\Id]
