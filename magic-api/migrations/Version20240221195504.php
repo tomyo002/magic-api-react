fcedb86ffee5c0ba346f865145368d15715652f7
Daret Tom <daret.tom@gmail.com>
2024-02-21 21:01:06 +0100

    creating new migrations

---
+++ magic-api/migrations/Version20240221195504.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240221195504 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, card_id INT DEFAULT NULL, picture LONGBLOB NOT NULL, UNIQUE INDEX UNIQ_16DB4F894ACC9A20 (card_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F894ACC9A20 FOREIGN KEY (card_id) REFERENCES card (id)');
+        $this->addSql('ALTER TABLE card ADD url VARCHAR(255) NOT NULL, ADD picture_url VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F894ACC9A20');
+        $this->addSql('DROP TABLE picture');
+        $this->addSql('ALTER TABLE card DROP url, DROP picture_url');
+    }
+}
