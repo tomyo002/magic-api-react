553fb0d94b3b4af7782c98aa3314a278e6f0adf6
Tom Daret <daret.tom@gmail.com>
2024-02-21 15:38:49 +0100

    adding button to redirect

--- magic-front/src/components/CardsList.jsx
+++ magic-front/src/components/CardsList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { fetchAllCards } from "../services/api/cards";
 import CostCard from "./CostCard";
+import {navigate} from "wouter/use-hash-location";
 
 function CardsList() {
   const [cardsData, setCardsData] = useState([]);
@@ -13,6 +14,7 @@ function CardsList() {
       <article className="card__header header__title article__card">
         {card.name} <div><CostCard costs={card.costs} /></div>
       </article>
+        <a href={`/card/${card.id}`}><button type="button" className='btn' >plus info</button></a>
     </div>
   ));
   return <div>{lstCards}</div>;
