e061d15e895c6a919c05cf1560ec02a53b339287
Daret Tom <daret.tom@gmail.com>
2024-02-27 11:10:19 +0100

    new route

--- magic-front/src/routes/Index.jsx
+++ magic-front/src/routes/Index.jsx
@@ -3,15 +3,25 @@ import React from "react";
 import { Route, useParams } from "wouter";
 import CardsList from "../components/CardsList";
 import CardsDetails from "../components/CardsDetails";
+import Navbar from "../components/Navbar";
+import KeywordList from "../components/KeywordList";
+import ExtensionList from "../components/ExtensionList";
 
 function Index() {
   return (
     <switch>
+      <Navbar />
       <Route path="/">
         <CardsList />
       </Route>
       <Route path="/card/:id">
         {(params) => <CardsDetails id={params.id} />}
+      </Route>
+      <Route path="/keyword">
+        <KeywordList />
+      </Route>
+      <Route path="/extension">
+        <ExtensionList />
       </Route>
     </switch>
   );
