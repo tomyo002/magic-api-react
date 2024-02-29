dc091951b0c680d847ae186060706625c4384e5d
Daret Tom <daret.tom@gmail.com>
2024-02-29 15:32:06 +0100

    new service

--- magic-front/src/services/api/Keywords.js
+++ magic-front/src/services/api/Keywords.js
@@ -12,3 +12,11 @@ export async function fetchKeyword(url) {
   const reponse = await fetch(`${BASE_URL}${url}`);
   return reponse.json();
 }
+export async function fetchKeywordwithId(id) {
+  const reponse = await fetch(`${BASE_URL}/api/keywords/${id}`);
+  return reponse.json();
+}
+export async function fetchCard(url) {
+  const reponse = await fetch(`${BASE_URL}${url}`);
+  return reponse.json();
+}
