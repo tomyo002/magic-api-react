50da3184012f0c07aa8aa5937f6d815e14da5980
Daret Tom <daret.tom@gmail.com>
2024-02-29 16:23:26 +0100

    eslint

--- magic-front/src/services/api/Capacities.js
+++ magic-front/src/services/api/Capacities.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "http://localhost:8000";
 
 export async function fetchCapacity(url) {
-    const reponse = await fetch(`${BASE_URL}${url}`);
-    return reponse.json();
+  const reponse = await fetch(`${BASE_URL}${url}`);
+  return reponse.json();
 }
