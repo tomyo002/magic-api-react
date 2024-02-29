41b584a8ddf19f7e34d6c817ac489339b10fa846
Daret Tom <daret.tom@gmail.com>
2024-02-29 15:55:02 +0100

    new service

--- magic-front/src/services/api/Extension.js
+++ magic-front/src/services/api/Extension.js
@@ -4,3 +4,12 @@ export async function fetchAllExtension() {
   const reponse = await fetch(`${BASE_URL}/api/extensions`);
   return reponse.json();
 }
+export async function fetchExtension(id) {
+  const reponse = await fetch(`${BASE_URL}/api/extensions/${id}`);
+  return reponse.json();
+}
+
+export async function fetchCard(url) {
+  const reponse = await fetch(`${BASE_URL}${url}`);
+  return reponse.json();
+}
